15a1ed5755610e733e45b5437fbede5de2d9ecab
Dan Gleason <dannycgleason@gmail.com>
2023-07-31 10:37:30 -0400

    update

--- src/pages/ManyWeekends.js
+++ src/pages/ManyWeekends.js
@@ -262,7 +262,7 @@ const ManyWeekends = () => {
         </div>
       </div>
       <div className="absolute top-[267px] left-[1184px] rounded-[3px] bg-royalblue w-[167px] flex flex-row p-2.5 box-border items-center justify-center text-white">
-        <b className="relative">{`Print 41 Letter Books `}</b>
+        <b className="relative">{`Print Palanca for all 20 retreatants`}</b>
       </div>
   </div>
   );
